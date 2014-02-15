fba45ec920a9ed534d2832c137e0c3ec4850ee67
Murilo Campaner <murilocampaner@MacBook-Pro-de-Murilo.local>
2014-02-15 15:25:09 -0200

    Init Projeto

---
+++ ClassAssistant/ClassAssistant/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ClassAssistant
+//
+//  Created by Murilo Campaner on 15/02/14.
+//  Copyright (c) 2014 Murilo Campaner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ ClassAssistant/ClassAssistantTests/ClassAssistantTests.m
@@ -0,0 +1,34 @@
+//
+//  ClassAssistantTests.m
+//  ClassAssistantTests
+//
+//  Created by Murilo Campaner on 15/02/14.
+//  Copyright (c) 2014 Murilo Campaner. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ClassAssistantTests : XCTestCase
+
+@end
+
+@implementation ClassAssistantTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
